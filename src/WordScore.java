078bdc82113187cc279a4535ff89c16e46375153
Kushagra8888 <kushagra8888@gmail.com>
2015-07-23 09:36:14 +0530

    working code

---
+++ src/WordScore.java
@@ -0,0 +1,97 @@
+/**
+ * Created by test on 7/23/2015.
+ */
+
+import java.io.BufferedReader;
+import java.io.*;
+import java.util.*;
+
+public class WordScore {
+
+    private String[] wordList;
+    private Map<Integer, ArrayList> map = new TreeMap(Collections.reverseOrder());
+
+    public WordScore(String path){
+        BufferedReader br = null;
+        try {
+            String sCurrentLine;
+            br = new BufferedReader(new FileReader(path));
+            while ((sCurrentLine = br.readLine()) != null) {
+                addToHash(computeWordScore(sCurrentLine), sCurrentLine);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (br != null)br.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    public int computeWordScore(String word){
+        int score = 0;
+        char[] wordChars = word.toCharArray();
+        for (int pos = 0; pos < wordChars.length; pos++) {
+            score += wordChars[pos] - 'a' + 1;
+        }
+        return score;
+    }
+
+    public void addToHash(Integer key, String val) {
+        if (map.containsKey(key)) {
+            ArrayList existingValues = map.get(key);
+            existingValues.add(val);
+            map.put(key, existingValues);
+        } else {
+            ArrayList<String> values = new ArrayList();
+            values.add(val);
+            map.put(key, values);
+        }
+    }
+
+    public String printList() {
+        String outputString = "";
+        Set set2 = map.entrySet();
+        Iterator iterator2 = set2.iterator();
+        while(iterator2.hasNext()) {
+            Map.Entry me2 = (Map.Entry) iterator2.next();
+            outputString += (me2.getKey() + ": ");
+            outputString += (me2.getValue() + "\n");
+        }
+        return outputString;
+    }
+
+    public void writeToFile(String path) {
+        try {
+
+            String content = printList();
+
+            File file = new File(path);
+
+            // if file doesnt exists, then create it
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
+            FileWriter fw = new FileWriter(file.getAbsoluteFile());
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(content);
+            bw.close();
+
+            System.out.println("Write complete!");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        WordScore wordscores = new WordScore(args[0]);
+        wordscores.printList();
+        wordscores.writeToFile(args[1]);
+    }
+}
